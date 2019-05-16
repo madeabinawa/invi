8b462add27b01f228259f153ba58e575e5a881f6
I Made Dwija Abinawa <madeabinawa@gmail.com>
2019-05-17 00:19:40 +0800

    change jquery

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Gugi|Roboto+Slab" rel="stylesheet">
     {{-- jquery --}}
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="../js/jquery.easing.1.3.js"></script>
     {{-- bootstrap css --}}
     {{ HTML::style('css/app.css') }}
     {{-- custom css --}}
@@ -157,7 +158,6 @@
 
     <!-- Optional JavaScript -->
     <script src="../js/script.js"></script>
-    <script src="../js/jquery.easing.1.3.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
